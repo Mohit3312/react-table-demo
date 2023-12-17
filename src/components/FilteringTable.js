e900df130a33992139650f039163b7f71b909616
Mohit Kumar <mohit3312@gmail.com>
2023-12-17 18:51:29 +0530

    Implemented Global Filtering in Table.

---
+++ src/components/FilteringTable.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { useMemo } from "react";
+import { COLUMNS } from "./columns";
+import MOCK_DATA from "./MOCK_DATA.json";
+import { useTable, useGlobalFilter } from "react-table";
+import "./table.css";
+import { GlobalFilter } from "./GlobalFilter";
+
+export const FilteringTable = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    footerGroups,
+    rows,
+    prepareRow,
+    state,
+    setGlobalFilter,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useGlobalFilter
+  );
+
+  const { globalFilter } = state;
+
+  return (
+    <>
+      <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((header) => (
+                <th {...header.getHeaderProps()}>{header.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+        <tfoot>
+          {footerGroups.map((footerGroup) => (
+            <tr {...footerGroup.getFooterGroupProps()}>
+              {footerGroup.headers.map((header) => (
+                <td {...header.getFooterProps()}>{header.render("Footer")}</td>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+    </>
+  );
+};
